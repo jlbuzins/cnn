17a9df63b43f99925b64e67e750d6d9ebba8d624
jlbuzins <133795983+jlbuzins@users.noreply.github.com>
2023-11-21 17:23:37 -0700

    Added comments to run-CNN.sh

--- run-CNN.sh
+++ run-CNN.sh
@@ -1,3 +1,5 @@
+# Edit these lines to specify other compute resources, including time. partition, cores, and GPU.
+# Time allocation should be in the form d-hh:mm:ss
 #!/bin/bash
 #SBATCH -p general -q public -t 7-0 -c 12 --mem=128G 
 #SBATCH -G a100:1 
@@ -5,8 +7,10 @@
 #SBATCH --export=NONE
 #SBATCH -o slurm.%j.out
 
+# Not specifying a CNN-variant (e.g. a or c) will result in an error
 variant="${1:?-ERROR MUST PASS A LETTER CODE ASSOCIATED WITH A VARIANT}"
 
+# Create a log directory if one does not already exist, and create a txt output file for it.
 ! [[ -d log ]] && mkdir -p log || :
 module load mamba/latest
 source activate tensorflow-gpu-2.10.0
